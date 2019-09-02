689bcabd41c7c3dce9b3cb635cf51c3c65f55a14
zhewen <zhewen.dev@gmail.com>
2019-09-02 22:32:38 +0800

    [D19902]TweenAnimationDemo

--- ViewAnimation/TweenAnimationDemo/app/src/main/java/com/baiheng/tweenanimationdemo/MainActivity.java
+++ ViewAnimation/TweenAnimationDemo/app/src/main/java/com/baiheng/tweenanimationdemo/MainActivity.java
@@ -3,12 +3,32 @@ package com.baiheng.tweenanimationdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mScaleBtn;
+    private Animation mScaleAnimation;
+    private TextView mContentTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScaleAnimation = AnimationUtils.loadAnimation(this,R.anim.scale);
+        mScaleBtn = (Button) findViewById(R.id.btn_scale_animation);
+        mContentTv = (TextView) findViewById(R.id.tv_content);
+
+        mScaleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mContentTv.startAnimation(mScaleAnimation);
+            }
+        });
     }
 }
